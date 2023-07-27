7c3a29019c9e3e2d3080b4db10a31e777210a86c
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-07-27 13:59:22 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -2,5 +2,24 @@
 var mongoose= require("mongoose");
 
 //write your schema Here with name of schema as productSchema
+var productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    Quantity: {
+        type: Number,
+        required: true,
+    },
+    Price: {
+        type: Number,
+        required: true,
+    },
+    instock: {
+        type: String,
+        enum: ['Yes', 'No'],
+        required: true,
+    },
+})
 
 module.exports =mongoose.model("products",productSchema);
